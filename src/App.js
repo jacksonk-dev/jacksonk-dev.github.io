cc5e84f61458c380ecd35f90f8ba1f7f8ca2a48f
je-kay <kamyaj89@gmail.com>
2019-01-14 20:26:26 +0300

    Styles the app a bit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Switch from 'react-router-dom/Switch';
 import Route from 'react-router-dom/Route';
+import withStyles from '@material-ui/core/styles/withStyles';
 import SideMenu from './components/side-menu';
 import Content from './components/content';
 
@@ -8,19 +10,29 @@ const styles = {
   root: {
     display: 'flex',
   },
+  content: {
+    padding: 32,
+    width: '100%',
+  },
 };
 
-const App = () => (
-  <div style={styles.root}>
+const App = ({ classes }) => (
+  <div className={classes.root}>
     <Switch>
       <Route path="/:active" component={({ match: { params } }) => (<SideMenu {...params} />)} />
       <Route path="" component={() => (<SideMenu active="profile" />)} />
     </Switch>
-    <Switch>
-      <Route path="/:item" component={({ match: { params } }) => (<Content {...params} />)} />
-      <Route path="" component={() => (<Content item="profile" />)} />
-    </Switch>
+    <div className={classes.content}>
+      <Switch>
+        <Route path="/:active" component={({ match: { params } }) => (<Content {...params} />)} />
+        <Route path="" component={() => (<Content active="profile" />)} />
+      </Switch>
+    </div>
   </div>
 );
 
-export default App;
+App.propTypes = {
+  classes: PropTypes.shape({}).isRequired,
+};
+
+export default withStyles(styles)(App);
