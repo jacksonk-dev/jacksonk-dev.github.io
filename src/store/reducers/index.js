689c5cf657c328ed6bba679afef1a14ea5e12e6c
je-kaypro <kamyaj89@gmail.com>
2019-04-03 08:48:14 +0300

    Adds online section to store profile

--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -1,11 +1,56 @@
+import {
+  FaLinkedin,
+  FaTwitter,
+  FaGithub,
+  FaInstagram,
+  FaFacebookF,
+} from 'react-icons/fa';
+
 const initialState = {
   profile: {
     name: 'je-kaypro',
     description: 'JavaScript and Python dev. Just a spoiler alert, am that guy you see in your dreams - the greatest Js & Python coder in your 2020 dreams.',
-    contacts: {
-      LinkedIn: 'https://www.linkedIn.com/in/jekaypro',
-      Yote: 'https://yote.app/@j.kamya',
-    },
+    image: './images/profilePic.jpg',
+    online: [
+      {
+        app: 'LinkedIn',
+        link: 'https://www.linkedIn.com/in/jekaypro',
+        Icon: FaLinkedin,
+        username: 'jekaypro',
+      },
+      {
+        app: 'Twitter',
+        link: 'https://www.twitter.com/jekaypro',
+        Icon: FaTwitter,
+        username: 'jekaypro',
+      },
+      {
+        app: 'Github',
+        link: 'https://github.com/je-kaypro',
+        Icon: FaGithub,
+        username: 'je-kaypro',
+      },
+      {
+        app: 'Instagram',
+        link: 'https://www.instagram.com/je_kaypro/',
+        Icon: FaInstagram,
+        username: 'je_kaypro',
+      },
+      {
+        app: 'Facebook',
+        link: 'https://www.facebook.com/kamya.jackson.50',
+        Icon: FaFacebookF,
+      },
+      {
+        app: 'Mixcloud',
+        link: 'https://www.mixcloud.com/Deeje_kay_UG',
+      },
+      {
+        app: 'Yote',
+        link: 'https://yote.app/@j.kamya',
+        username: 'j.kamya',
+      },
+    ],
   },
 
   projects: [
