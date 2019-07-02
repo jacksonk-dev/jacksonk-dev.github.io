ae9b25020c40ca75486884971c490f854a287218
je-kaypro <kamyaj89@gmail.com>
2019-07-02 10:35:19 +0300

    Small changes to index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Router from 'react-router-dom/BrowserRouter';
+import { BrowserRouter as Router } from 'react-router-dom';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import reducers from './store/reducers';
