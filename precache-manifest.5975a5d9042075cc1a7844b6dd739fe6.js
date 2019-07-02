72b91cbb972727ad9e0e24bd88034ccac38dda43
je-kaypro <kamyaj89@gmail.com>
2019-07-02 10:24:38 +0300

    Updates

---
+++ precache-manifest.5975a5d9042075cc1a7844b6dd739fe6.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ca0bebd91754d799823b73d446fe300",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2576b5ecf533ee6fd092",
+    "url": "/static/css/main.059246eb.chunk.css"
+  },
+  {
+    "revision": "18bf6befab59a61b7c58",
+    "url": "/static/js/2.982372b2.chunk.js"
+  },
+  {
+    "revision": "2576b5ecf533ee6fd092",
+    "url": "/static/js/main.60ca08cd.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "06e36d02e76f1eaf481b1832a23c4335",
+    "url": "/static/media/sexy_beachy-webfont.06e36d02.woff"
+  },
+  {
+    "revision": "8317f97dc19066831d9ca92427e8a7da",
+    "url": "/static/media/sexy_beachy-webfont.8317f97d.ttf"
+  },
+  {
+    "revision": "0d4125deb770bb51355b620ca5d42197",
+    "url": "/static/media/the_bredan.0d4125de.ttf"
+  },
+  {
+    "revision": "40baa67821e413c9fe1a69af8e80b761",
+    "url": "/static/media/the_bredan.40baa678.svg"
+  },
+  {
+    "revision": "6ae1551de3ae8843005588398e79c585",
+    "url": "/static/media/the_bredan.6ae1551d.woff"
+  }
+]);
